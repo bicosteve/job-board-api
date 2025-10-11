726e9ab3e278a87f0f0b911f17755406d1ef43dd
bsteve <bico.steve@betika.com>
2025-10-11 20:44:00 +0300

    added request parser for login endpoint

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, reqparse
 from ..services.user_service import UserService
 
 register_parser = reqparse.RequestParser()
-
 register_parser.add_argument("email", required=True, help="Email is required")
 register_parser.add_argument("password", required=True, help="Password is required")
 register_parser.add_argument(
@@ -32,7 +31,7 @@ class UserRegister(Resource):
 
 class UserLogin(Resource):
     def post(self):
-        data = register_parser.parse_args()
+        data = login_parser.parse_args()
         try:
             user = UserService.create_user(data["username"])
             return user, 200
