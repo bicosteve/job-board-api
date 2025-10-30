eecd4f7461888d763596d78c5c74014c806bf239
bsteve <bico.steve@betika.com>
2025-10-30 05:14:34 +0300

    added test for register and login controllers

--- tests/test_register_controller.py
+++ tests/test_register_controller.py
@@ -1,9 +1,10 @@
 import unittest
 from unittest.mock import patch
 from flask import Flask, json
+from marshmallow import ValidationError
 
 from app.controllers.user_controllers import UserRegister
-from app.utils.exceptions import UserExistError, GenericDatabaseError
+from app.utils.exceptions import UserExistError
 
 
 class TestRegisterController(unittest.TestCase):
@@ -15,15 +16,52 @@ class TestRegisterController(unittest.TestCase):
         self.app.add_url_rule(
             '/register', view_func=UserRegister.as_view('register'))
 
+        self.email = 'john.doe@example.com'
+        self.password = 'secure1234'
+
         self.payload = {
-            'email': 'john.doe@example.com',
-            'password': 'secure1234',
+            'email': self.email,
+            'password': self.password,
             'confirm_password': 'secure1234'
         }
 
+    def test_register_validation_error(self):
+        '''Should return 400 if the schema validation fails'''
+        target = 'app.controllers.user_controllers.UserService.register_user'
+        payload = {
+            'email': self.email,
+            'password': self.password
+        }
+        with patch(target) as mock_register:
+            mock_register.side_effect = ValidationError('validation error')
+            response = self.client.post(
+                '/register',
+                data=json.dumps(payload),
+                content_type='application/json'
+            )
+
+            # Assertion
+            self.assertEqual(response.status_code, 400)
+            data = json.loads(response.data)
+            self.assertIn('error', data)
+
+    def test_register_user_already_exists(self):
+        '''Should return 400 if user already exists'''
+        target = 'app.controllers.user_controllers.UserService.register_user'
+        with patch(target) as mock_register:
+            mock_register.side_effect = UserExistError('user already exists')
+            response = self.client.post(
+                '/register',
+                data=json.dumps(self.payload),
+                content_type='application/json'
+            )
+
+            self.assertEqual(response.status_code, 400)
+            data = json.loads(response.data)
+            self.assertIn('user_error', data)
+
     def test_register_user_success(self):
         '''Should return 201 when user is successfully registered'''
-
         with patch('app.controllers.user_controllers.UserService.register_user') as mock_register, \
                 patch('app.controllers.user_controllers.UserService.store_verification_code') as mock_store, \
                 patch('app.controllers.user_controllers.Helpers.generate_verification_code') as mock_code, \
