de67958ebfbd25901cae85e29e3197451dbc544e
bsteve <bicosteve4@gmail.com>
2025-11-08 05:26:20 +0300

    fixed the redis password error

--- app/db/redis.py
+++ app/db/redis.py
@@ -1,4 +1,3 @@
-import os
 import redis
 from flask import current_app, g
 
@@ -7,24 +6,25 @@ class Cache:
 
     @staticmethod
     def connect_redis():
-        '''
+        """
         Return a Redis client bound to the current context
         Reuse it if already exists in global request context (g)
         (g) only exists for a lifetime of a request.
-        '''
-        if 'redis' not in g:
+        """
+        if "redis" not in g:
+            password = current_app.config["REDIS_PASSWORD"]
             g.redis = redis.Redis(
                 host=current_app.config["REDIS_HOST"],
                 port=current_app.config["REDIS_PORT"],
                 db=current_app.config["REDIS_DB"],
-                password=current_app.config['REDIS_PASSWORD'],
+                password=password if password else None,
                 decode_responses=True,
             )
         return g.redis
 
     @staticmethod
     def close_redis(e=None):
-        '''Close Redis connection at the end of request'''
-        client = g.pop('redis', None)
+        """Close Redis connection at the end of request"""
+        client = g.pop("redis", None)
         if client is not None:
             client.close()
