7ec7c58bed0b2c3e4c6042246d4c1a42203af7f7
bsteve <bico.steve@betika.com>
2025-12-02 05:45:44 +0300

    added profile and education controllers

--- app/schemas/profile.py
+++ app/schemas/profile.py
@@ -15,8 +15,3 @@ class ProfileSchema(Schema):
             'error': 'required and must be between 2 and 100 characters'}
     )
     cv_url = fields.Str(required=False, allow_none=True)
-    user_id = fields.Int(
-        required=True,
-        validate=[validate.Length(min=1)],
-        error_messages={'error': 'required,int and minimum value is 1'}
-    )
